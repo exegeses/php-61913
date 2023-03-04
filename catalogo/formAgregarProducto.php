1f1a34ff32bb157656db39414ce06a08fc61dbb1
Marcos <exegeses@gmail.com>
2023-03-03 21:20:38 -0300

    formulario de alta de una nuevo producto

--- catalogo/formAgregarProducto.php
+++ catalogo/formAgregarProducto.php
@@ -4,14 +4,14 @@
 	include 'layout/nav.php';
 ?>
 
-    <main class="container">
+    <main class="container py-4">
 
         <h1>Alta de un nuevo producto</h1>
 
         <div class="alert bg-light p-4 col-8 mx-auto border shadow-sm">
             <form action="agregarProducto.php" method="post" enctype="multipart/form-data">
 
-                <div class="form-group">
+                <div class="form-grou mb-2">
                     <label for="prdNombre">Nombre del Producto</label>
                     <input type="text" name="prdNombre" class="form-control" id="prdNombre" required>
                 </div>
@@ -24,29 +24,25 @@
                     <input type="number" name="prdPrecio" class="form-control" id="prdPrecio" min="0" step="0.01" required>
                 </div>
 
-                <div class="form-group">
+                <div class="form-group mb-2">
                     <label for="idMarca">Marca</label>
                     <select class="form-control" name="idMarca" id="idMarca" required>
                         <option value="">Seleccione una marca</option>
                     </select>
                 </div>
 
-                <div class="form-group">
+                <div class="form-group mb-2">
                     <label for="idCategoria">Categoría</label>
                     <select class="form-control" name="idCategoria" id="idCategoria" required>
                         <option value="">Seleccione una categoría</option>
                     </select>
                 </div>
 
-                <div class="form-group">
-                    <label for="prdPresentacion">Presentación del Producto</label>
-                    <textarea name="prdPresentacion" class="form-control" id="prdPresentacion" required></textarea>
+                <div class="form-group mb-2">
+                    <label for="prdDescripcion">Presentación del Producto</label>
+                    <textarea name="prdDescripcion" class="form-control" id="prdDescripcion" required></textarea>
                 </div>
 
-                <div class="form-group">
-                    <label for="prdStock">Stock del Producto</label>
-                    <input type="number" name="prdStock" class="form-control" id="prdStock" min="0" required>
-                </div>
 
                 <div class="custom-file mt-1 mb-4">
                     <input type="file" name="prdImagen"  class="custom-file-input" id="customFileLang" lang="es">
