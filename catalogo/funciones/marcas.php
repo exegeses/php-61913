f571fd5c6668fcf216c6b6ff39a331533fe1baa5
Marcos <exegeses@gmail.com>
2023-03-08 21:19:08 -0300

    dudas acerca de log de errores

--- catalogo/funciones/marcas.php
+++ catalogo/funciones/marcas.php
@@ -4,14 +4,37 @@
     function listarMarcas() : mysqli_result | false
     {
         $link = conectar();
-        $sql = "SELECT * FROM marcas";
+        $sql = "SELECT * FROM marcasX";
         try {
             $resultado = mysqli_query( $link, $sql );
+            /* log de errores */
+            $arch = 'log/marcas/logAdminMarcas.txt';
+            $logAdminMarcas = fopen($arch, 'a');
+            $contenido = date('d/m/Y H:i:s')."\n";
+            $contenido .= 'mensaje: '.mysqli_error($link)."\n";
+            $contenido .= 'Archivo: '.$e->getFile()."\n";
+            $contenido .= 'Nro Línea: '.$e->getLine()."\n";
+            $contenido .= 'Mensaje: '.$e->getMessage()."\n";
+            $contenido .= '----------------------'."\n";
+            fwrite($logAdminMarcas, $contenido );
+            fclose($logAdminMarcas);
+            //echo $e->getMessage();
             return $resultado;
         }
         catch ( Exception $e ){
             /* log de errores */
+           /* $arch = 'log/marcas/logAdminMarcas.txt';
+            $logAdminMarcas = fopen($arch, 'a');
+            $contenido = date('d/m/Y H:i:s')."\n";
+            //$contenido .= 'mensaje: '.mysqli_error($link)."\n";
+            $contenido .= 'Archivo: '.$e->getFile()."\n";
+            $contenido .= 'Nro Línea: '.$e->getLine()."\n";
+            $contenido .= 'Mensaje: '.$e->getMessage()."\n";
+            $contenido .= '----------------------'."\n";
+            fwrite($logAdminMarcas, $contenido );
+            fclose($logAdminMarcas);*/
             echo $e->getMessage();
+            echo 'NO va a ejecutar el log del try, una vez más';
             $resultado = false;
         }
         return $resultado;
